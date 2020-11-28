3f065f190622a869af18a00c60586068cc0419f5
sorencoder <69846992+sorencoder@users.noreply.github.com>
2020-11-28 16:31:27 +0530

    Create simple_interest.c

---
+++ simple_interest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int p,r,t;
+    float si=1;
+    printf("Enter the principal amount: ");
+    scanf("%d",&p);
+    printf("Enter the rate: ");
+    scanf("%d",&r);
+    printf("Enter the time: ");
+    scanf("%d",&t);
+    si=p*r*t/100;
+    printf("Simple Interest: %f",si);
+    return 0;
+}
