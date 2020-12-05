8ed810c2ea5dfcab0e00454b7980485441117128
sorencoder <69846992+sorencoder@users.noreply.github.com>
2020-12-05 20:34:06 +0530

    Update simple_interest.c

--- simple_interest.c
+++ simple_interest.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-
 int main()
 {
-    int p,r,t;
-    float si=1;
+    int p,t;
+    float r,si=1;
     printf("Enter the principal amount: ");
     scanf("%d",&p);
     printf("Enter the rate: ");
-    scanf("%d",&r);
-    printf("Enter the time: ");
+    scanf("%f",&r);
+    printf("Enter the time in years: ");
     scanf("%d",&t);
     si=p*r*t/100;
-    printf("Simple Interest: %.2f",si);
+    printf("Simple Interest: Rs. %.2f",si);
     return 0;
 }
