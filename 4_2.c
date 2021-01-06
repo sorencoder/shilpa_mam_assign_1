0bfd802322bbf352bdee5d724c44fd6dae3c1b1b
sorencoder <69846992+sorencoder@users.noreply.github.com>
2021-01-06 11:57:01 +0530

    Create 4_2.c

---
+++ 4_2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    char str[20];
+    char grade;
+    int sub1;
+    int sub2;
+    int sub3;
+    int sub4;
+    int sub5;
+    int total;
+    float avg;
+    // printf("Enter the name of the student: ");
+    // gets(str);
+    printf("Enter the marks of Mathematics ");
+    scanf("%d",&sub1);
+    // printf("Enter the marks of Physics ");
+    // scanf("%d",&sub2);
+    // printf("Enter the marks of Chemistry ");
+    // scanf("%d",&sub3);
+    // printf("Enter the marks of Biology ");
+    // scanf("%d",&sub4);
+    // printf("Enter the marks of English ");
+    // scanf("%d",&sub5);
+    // total=sub1+sub2+sub3+sub4+sub5;
+    // avg=(float)total/5;
+    // if(avg>=90)
+    // grade='A';
+    // if(avg<90 && avg>=60)
+    // grade='B';
+    // printf("%d",total);
+    // printf("%.2f",avg);
+    // printf("%c",grade);
+    printf("%d",sub1);
+    
+    return 0;
+}
