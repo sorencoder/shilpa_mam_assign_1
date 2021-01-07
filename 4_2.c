fef8d872e1ee543f6f1826f5710d6f8eec45f925
sorencoder <69846992+sorencoder@users.noreply.github.com>
2021-01-07 15:01:48 +0530

    Update 4_2.c

--- 4_2.c
+++ 4_2.c
@@ -8,30 +8,34 @@ int main()
     int sub3;
     int sub4;
     int sub5;
-    int total;
-    float avg;
-    // printf("Enter the name of the student: ");
-    // gets(str);
+    int total=0;
+    float avg=0.00;
+    printf("Enter the name of the student: ");
+    gets(str);
     printf("Enter the marks of Mathematics ");
     scanf("%d",&sub1);
-    // printf("Enter the marks of Physics ");
-    // scanf("%d",&sub2);
-    // printf("Enter the marks of Chemistry ");
-    // scanf("%d",&sub3);
-    // printf("Enter the marks of Biology ");
-    // scanf("%d",&sub4);
-    // printf("Enter the marks of English ");
-    // scanf("%d",&sub5);
-    // total=sub1+sub2+sub3+sub4+sub5;
-    // avg=(float)total/5;
-    // if(avg>=90)
-    // grade='A';
-    // if(avg<90 && avg>=60)
-    // grade='B';
-    // printf("%d",total);
-    // printf("%.2f",avg);
-    // printf("%c",grade);
-    printf("%d",sub1);
-    
+    printf("Enter the marks of Physics ");
+    scanf("%d",&sub2);
+    printf("Enter the marks of Chemistry ");
+    scanf("%d",&sub3);
+    printf("Enter the marks of Biology ");
+    scanf("%d",&sub4);
+    printf("Enter the marks of English ");
+    scanf("%d",&sub5);
+    total=sub1+sub2+sub3+sub4+sub5;
+    avg=(float)total/5;
+    if(avg>=90)
+    grade='A';
+    else if(avg<90 && avg>=60)
+    grade='B';
+    else if(avg<60 && avg>=40)
+    grade='C';
+    else if(avg<40 && avg>=30)
+    grade='D';
+    else grade='F';
+    printf("Name of the student: %s",str);
+    printf("\nTotal marks obtained: %d",total);
+    printf("\nAverage: %.2f",avg);
+    printf("\nGrade: %c",grade);
     return 0;
 }
