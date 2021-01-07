c0f7a2a30d6f76dffeb5e5b61436bc5654a602fa
sorencoder <69846992+sorencoder@users.noreply.github.com>
2021-01-07 15:04:57 +0530

    Create 5_1.c

---
+++ 5_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    int i;
+    printf("Enter the value of n ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d",i);
+        printf("\n");
+    }
+    return 0;
+}
