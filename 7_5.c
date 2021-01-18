2e34633aa643e039ab7994b4a87b2cc4317f8783
sorencoder <69846992+sorencoder@users.noreply.github.com>
2021-01-18 20:37:04 +0530

    Create 7_5.c

---
+++ 7_5.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j;
+    int n;
+    int k = 0;
+    printf("Enter the number of row you want: ");
+    scanf("%d",&n);
+    for ( i = 1; i <=n; i++,k=0)
+    {
+        for ( j = 1; j <=n-i; j++)
+        {
+             printf("  ");  //two space given
+        }
+        while(k!=2*i-1)
+        {
+            printf("* ");
+            k++;
+        }
+        
+        printf("\n");
+        
+    }
+return 0;
+}
